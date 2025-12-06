489efa7e32b09aa55056677d1a9f4c63f4ef0588
Abusaleh Alam Khan <abusaleh.uiu.cse@gmail.com>
2025-12-06 22:47:01 +0600

    Refactor callback function example for clarity and consistency

--- anonymous_vs_callbackFunction.js
+++ anonymous_vs_callbackFunction.js
@@ -7,3 +7,14 @@ const add = function(a,b){
 
 result = add(7,9);
 console.log(result);
+
+// callback function = it uses as a urgument of another function & executes later
+
+function greet(name, callback) {
+    console.log("Hello " + name);
+    callback(); // function ke call kora holo
+}
+
+greet("Rahim", function() {
+    console.log("How are you?");
+});
