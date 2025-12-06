94be8ed7ea3ba1ceba28ef682739cc77332098f1
Abusaleh Alam Khan <abusaleh.uiu.cse@gmail.com>
2025-12-06 22:39:04 +0600

    Add anonymous function example with callback usage

---
+++ anonymous_vs_callbackFunction.js
@@ -0,0 +1,9 @@
+// anonymous function = No name of this function
+// purpose : pass as a callback function / use to create arrow function
+
+const add = function(a,b){
+    return a*b;
+}
+
+result = add(7,9);
+console.log(result);
