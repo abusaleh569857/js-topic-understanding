d6d7fbc3f3caebd0d8debca9d28bb9021e31d7a5
Abusaleh Alam Khan <abusaleh.uiu.cse@gmail.com>
2025-12-27 16:30:51 +0600

    Updated monthly payment in payment array

--- saving.js
+++ saving.js
@@ -1,28 +1,24 @@
-function monthlySavings(payments,monthlyCost){
-    if (!Array.isArray(payments) || typeof monthlyCost!== 'number'){
-        return 'invalid input';
-    }
-    else{
-        let totalCost = 0;
-        for(let payment of payments){
-        if (payment < 3000){
-            totalCost += payment;
-        }
-        else {
-            payment = payment -((payment*20)/100);
-            totalCost += payment;
-
-        }
-
+function monthlySavings(payments, monthlyCost) {
+  if (!Array.isArray(payments) || typeof monthlyCost !== "number") {
+    return "invalid input";
+  } else {
+    let totalCost = 0;
+    for (let payment of payments) {
+      if (payment < 3000) {
+        totalCost += payment;
+      } else {
+        payment = payment - (payment * 20) / 100;
+        totalCost += payment;
+      }
     }
     let savings = totalCost - monthlyCost;
-   if (savings >= 0){
-    return savings;
-   }
-   return 'Earn More'
+    if (savings >= 0) {
+      return savings;
     }
+    return "Earn More";
+  }
 }
-let payments =  [ 900 , 2700 , 3400];
+let payments = [900, 2700, 3400, 6500];
 let monthlyCost = 5000;
-let totalSavings = monthlySavings(payments,monthlyCost);
+let totalSavings = monthlySavings(payments, monthlyCost);
 console.log(totalSavings);
