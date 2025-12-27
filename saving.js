38ac3d69dc2dea547633fc5d53222dcff7103e34
Abusaleh Alam Khan <abusaleh.uiu.cse@gmail.com>
2025-12-27 16:19:06 +0600

    Implement monthly savings calculation function with input validation

---
+++ saving.js
@@ -0,0 +1,28 @@
+function monthlySavings(payments,monthlyCost){
+    if (!Array.isArray(payments) || typeof monthlyCost!== 'number'){
+        return 'invalid input';
+    }
+    else{
+        let totalCost = 0;
+        for(let payment of payments){
+        if (payment < 3000){
+            totalCost += payment;
+        }
+        else {
+            payment = payment -((payment*20)/100);
+            totalCost += payment;
+
+        }
+
+    }
+    let savings = totalCost - monthlyCost;
+   if (savings >= 0){
+    return savings;
+   }
+   return 'Earn More'
+    }
+}
+let payments =  [ 900 , 2700 , 3400];
+let monthlyCost = 5000;
+let totalSavings = monthlySavings(payments,monthlyCost);
+console.log(totalSavings);
